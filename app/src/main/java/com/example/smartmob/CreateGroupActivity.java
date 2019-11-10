2a7fb771c61360b23d900da36c8b5da3466a3206
Thanapat <thanapat.jutha@gmail.com>
2019-11-10 16:54:12 +0700

    init mChatManager

--- app/src/main/java/com/example/smartmob/CreateGroupActivity.java
+++ app/src/main/java/com/example/smartmob/CreateGroupActivity.java
@@ -5,17 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.UUID;
+
 public class CreateGroupActivity extends AppCompatActivity {
 
-//    private ChatManager mChatManager;
+    private ChatManager mChatManager;
+
     EditText titleInput;
     EditText messageInput;
+    TextView groupPin;
 
-    protected void onSend(String title, String message){
-//        mChatManager.send()
+    protected void onSend(ChatMessage message){
+//        mChatManager.sendMessage(message);
         titleInput.setText("");
         messageInput.setText("");
     }
@@ -26,21 +31,32 @@ public class CreateGroupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_group);
 
         Intent intent = getIntent();
-//        String pin = intent.getExtras().getString("pin");
+        final String pin = intent.getExtras().getString("pin");
 
         titleInput = findViewById(R.id.title);
         messageInput = findViewById(R.id.messageInput);
+        groupPin = findViewById(R.id.groupPin);
 
         final String titleText = titleInput.getText().toString();
         final String messageText = messageInput.getText().toString();
 
+        groupPin.setText(pin);
+
+        mChatManager = new ChatManager(pin);
+//        mChatManager.start();
+
         final Button sendButton = findViewById(R.id.createGroupButton);
         sendButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                onSend(titleText, messageText);
+                ChatMessage message = new ChatMessage(UUID.randomUUID().toString(), pin, titleText, messageText);
+                onSend(message);
             }
         });
+    }
 
-//        mChatManager = new ChatManager(pin);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mChatManager.stop();
     }
 }
