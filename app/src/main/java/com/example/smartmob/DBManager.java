bcb6702e3c1c436e905257f540cf17dc5a6f5e32
Thanapat <thanapat.jutha@gmail.com>
2019-11-10 15:46:17 +0700

    create DBManager

---
+++ app/src/main/java/com/example/smartmob/DBManager.java
@@ -0,0 +1,87 @@
+package com.example.smartmob;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBManager extends SQLiteOpenHelper {
+
+    private static DBManager instance;
+
+    private static final String DB_NAME = "announcementLog";
+    private static final int DB_VERSION = 2;
+    private static final String TABLE_NAME = "announcement";
+    private static final String COL_PIN = "pin";
+    private static final String COL_MESSAGE_ID = "messageId";
+
+    private DBManager() {
+        super(AppApplication.getInstance(), DB_NAME, null, DB_VERSION);
+    }
+
+    static DBManager getInstance(){
+        if(instance == null){
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME +" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_PIN + " TEXT, "
+                + COL_MESSAGE_ID + " TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    boolean findMessage(ChatMessage message){
+
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.query
+                (TABLE_NAME, null,  COL_MESSAGE_ID+" =?", new String[]{message.getId().toString()}, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        while(!(cursor != null && cursor.isAfterLast())) {
+            String cpin = cursor.getString(1);
+
+            if (cpin.equals(message.getPin().toString())){
+                sqLiteDatabase.close();
+                return true;
+            }
+            cursor.moveToNext();
+        }
+
+        sqLiteDatabase.close();
+        return false;
+    }
+
+    public boolean addMessage(ChatMessage chatMessage){
+        if(findMessage(chatMessage)){
+            return false;
+        }
+
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COL_PIN, chatMessage.getPin());
+        values.put(COL_MESSAGE_ID, chatMessage.getId());
+
+        sqLiteDatabase.insert(TABLE_NAME, null, values);
+        sqLiteDatabase.close();
+        return true;
+    }
+
+    public void clearDB(){
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        sqLiteDatabase.delete(TABLE_NAME,null,null);
+        sqLiteDatabase.close();
+    }
+
+}
