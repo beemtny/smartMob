5401aa34854ed8c4dc2703b59134f66f19e1b811
Thanapat <thanapat.jutha@gmail.com>
2019-11-10 15:46:44 +0700

    create ChatMessage

--- app/src/main/java/com/example/smartmob/ChatMessage.java
+++ app/src/main/java/com/example/smartmob/ChatMessage.java
@@ -1,4 +1,55 @@
 package com.example.smartmob;
 
-public class ChatMessage {
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+class ChatMessage{
+
+    private String id;
+    private String title;
+    private String text;
+    private String pin;
+    private Date createdAt;
+
+    public ChatMessage(String id, String pin, String title, String text) {
+        this(id, pin, title, text, new Date());
+    }
+
+    public ChatMessage(String id, String pin, String title, String text, Date createdAt) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.pin = pin;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+//    private void writeObject(ObjectOutputStream out) throws IOException {
+//        out.defaultWriteObject();
+//    }
+//
+//    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+//        in.defaultReadObject();
+//    }
 }
