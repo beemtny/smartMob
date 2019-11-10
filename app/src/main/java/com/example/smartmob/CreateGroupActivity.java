d9f92e6daf9a644f90208cd7455e85c4b202ee2e
Thanapat <thanapat.jutha@gmail.com>
2019-11-10 19:24:39 +0700

    fix get text bug

--- app/src/main/java/com/example/smartmob/CreateGroupActivity.java
+++ app/src/main/java/com/example/smartmob/CreateGroupActivity.java
@@ -1,11 +1,13 @@
 package com.example.smartmob;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,13 +16,16 @@ import java.util.UUID;
 public class CreateGroupActivity extends AppCompatActivity {
 
     private ChatManager mChatManager;
+    private Context context;
 
     EditText titleInput;
     EditText messageInput;
     TextView groupPin;
 
     protected void onSend(ChatMessage message){
-//        mChatManager.sendMessage(message);
+        mChatManager.sendMessage(message);
+        Toast.makeText(context,"Send message with title: " + message.getTitle() + " 000.",Toast.LENGTH_SHORT).show();
+
         titleInput.setText("");
         messageInput.setText("");
     }
@@ -37,17 +42,18 @@ public class CreateGroupActivity extends AppCompatActivity {
         messageInput = findViewById(R.id.messageInput);
         groupPin = findViewById(R.id.groupPin);
 
-        final String titleText = titleInput.getText().toString();
-        final String messageText = messageInput.getText().toString();
-
         groupPin.setText(pin);
 
         mChatManager = new ChatManager(pin);
         mChatManager.start();
 
+        context = AppApplication.getInstance().getContext();
+
         final Button sendButton = findViewById(R.id.createGroupButton);
         sendButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                final String titleText = titleInput.getText().toString().trim();
+                final String messageText = messageInput.getText().toString().trim();
                 ChatMessage message = new ChatMessage(UUID.randomUUID().toString(), pin, titleText, messageText);
                 onSend(message);
             }
