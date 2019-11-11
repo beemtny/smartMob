e71257771f1bb83ed6b01e0eb5e5474c202f7bb0
Thanapat <thanapat.jutha@gmail.com>
2019-11-12 01:31:45 +0700

    db file

--- app/src/main/java/com/example/smartmob/DBManager.java
+++ app/src/main/java/com/example/smartmob/DBManager.java
@@ -5,6 +5,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
 public class DBManager extends SQLiteOpenHelper {
 
     private static DBManager instance;
@@ -13,6 +17,8 @@ public class DBManager extends SQLiteOpenHelper {
     private static final int DB_VERSION = 2;
     private static final String TABLE_NAME = "announcement";
     private static final String COL_PIN = "pin";
+    private static final String COL_TITLE = "title";
+    private static final String COL_MESSAGE = "message";
     private static final String COL_MESSAGE_ID = "messageId";
 
     private DBManager() {
@@ -30,6 +36,8 @@ public class DBManager extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + TABLE_NAME +" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + COL_PIN + " TEXT, "
+                + COL_TITLE + " TEXT, "
+                + COL_MESSAGE + " TEXT, "
                 + COL_MESSAGE_ID + " TEXT)");
     }
 
@@ -68,6 +76,8 @@ public class DBManager extends SQLiteOpenHelper {
 
         ContentValues values = new ContentValues();
         values.put(COL_PIN, chatMessage.getPin());
+        values.put(COL_TITLE, chatMessage.getTitle());
+        values.put(COL_MESSAGE, chatMessage.getText());
         values.put(COL_MESSAGE_ID, chatMessage.getId());
 
         sqLiteDatabase.insert(TABLE_NAME, null, values);
@@ -79,6 +89,35 @@ public class DBManager extends SQLiteOpenHelper {
         SQLiteDatabase sqLiteDatabase = getWritableDatabase();
         sqLiteDatabase.delete(TABLE_NAME,null,null);
         sqLiteDatabase.close();
+    }
+
+    List<ChatMessage> fetchChatMessageList(){
+        List<ChatMessage> messages = new ArrayList<>();
+
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        Cursor cursor = sqLiteDatabase.query
+                (TABLE_NAME, null, null, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        while(!(cursor != null && cursor.isAfterLast())) {
+
+            ChatMessage chatMessage = new ChatMessage(
+                    cursor.getString(4),
+                    cursor.getString(1),
+                    cursor.getString(2),
+                    cursor.getString(3)
+            );
+
+            messages.add(chatMessage);
+            cursor.moveToNext();
+        }
+
+        sqLiteDatabase.close();
+        return messages;
     }
 
 }
