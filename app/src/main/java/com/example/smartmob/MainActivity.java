04cd3d0cb98c289af23cbe092ed8dc80ffa5ec46
Thanapat <thanapat.jutha@gmail.com>
2019-11-12 22:41:19 +0700

    add ger perm

--- app/src/main/java/com/example/smartmob/MainActivity.java
+++ app/src/main/java/com/example/smartmob/MainActivity.java
@@ -7,9 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.Manifest;
+import android.provider.Settings;
+import androidx.core.content.ContextCompat;
+import 	androidx.core.app.ActivityCompat;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int MY_PERMISSIONS_MANAGE_WRITE_SETTINGS = 100 ;
+    static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 69;
+
+    private boolean mLocationPermission = false;
+    private boolean mSettingPermission = true;
 
     private String randomPin(){
         Random rand = new Random();
@@ -35,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_and_create);
 
+        settingPermission();
+        locationsPermission();
+
         final Button button = findViewById(R.id.joinNewGroupButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -48,5 +65,46 @@ public class MainActivity extends AppCompatActivity {
                 onCreateGroupBtn();
             }
         });
+    }
+
+    private void settingPermission() {
+        mSettingPermission = true;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(getApplicationContext())) {
+                mSettingPermission = false;
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, MY_PERMISSIONS_MANAGE_WRITE_SETTINGS);
+            }
+        }
+    }
+
+
+    private void locationsPermission(){
+        mLocationPermission = true;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            mLocationPermission = false;
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 }
